876f04cd00b2c28a46310a25ccd5867604e97bb4
ApoorvaAman <apoorvaamankvs@gmail.com>
2021-06-17 09:44:13 +0530

    Simple Error Message

--- src/Components/Error.tsx
+++ src/Components/Error.tsx
@@ -1,5 +1,7 @@
 const Error = (): JSX.Element => {
-  return <h1>Error</h1>
+  return (
+    <h1 className="error">Something unexpected happened! Please try again.</h1>
+  )
 }
 
 export default Error
